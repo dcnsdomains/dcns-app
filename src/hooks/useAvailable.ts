e5368222f0244d965d2d06a153a73b76667163f8
tomoki_sun <tomoki_69386@yahoo.co.jp>
2023-03-23 14:29:40 +0900

    update useAvailable.ts

--- src/hooks/useAvailable.ts
+++ src/hooks/useAvailable.ts
@@ -4,6 +4,12 @@ import { useContractAddress } from './useContractAddress'
 import { useProvider } from './useProvider'
 
 export const useAvailable = (normalisedName: string) => {
+  if (!normalisedName) {
+    return {
+      available: false,
+      isLoading: false,
+    }
+  }
   const name = normalisedName.split('.')[0]
   const provider = useProvider()
   const controllerAddr = useContractAddress('DcRegistrarController')
